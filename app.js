62af715a603986b8ef22e93dcadc800fab779005
DEEPESH001 <32238571+DEEPESH001@users.noreply.github.com>
2020-09-11 14:27:06 +0530

    commit

--- app.js
+++ app.js
@@ -57,6 +57,6 @@ app.post('/weather', function (req, res) {
   });
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('App running on port 3000');
 });
